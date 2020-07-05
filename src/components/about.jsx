1f1108299a7400ff44c3452bc9656924b5698788
Shubhendu <shubhendushishir@gmail.com>
2020-07-05 19:25:22 +0530

    Building own portfolio

---
+++ src/components/about.jsx
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+export default class About extends Component {
+  render() {
+    let resumeData = this.props.resumeData;
+    return (
+      <section id="about">
+         <div className="row">
+
+            <div className="three columns">
+
+               <img className="profile-pic"  src="images/profilepic.jpg" alt="" />
+
+            </div>
+
+            <div className="nine columns main-col">
+
+               <h2>About Me</h2>
+               <p>
+               {
+                 resumeData.aboutme
+               }
+               </p>
+
+               <div className="row">
+
+                  <div className="columns contact-details">
+
+                  <h2>Contact Details</h2>
+                  <p className="address">
+       					<span>{resumeData.name}</span>
+                        <br></br>
+                        <span>{resumeData.emailId}</span>
+                        <br></br>
+       					<span>{resumeData.address} </span>
+                    <br></br>
+       			  </p>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </section>
+    );
+  }
+}
---
+++ src/resumeData.jsx
@@ -0,0 +1,76 @@
+let resumeData = {
+    "imagebaseurl":"https://techonoeticsclub.github.io/",
+    "name": "Shubhendu Shishir",
+    "role": "Senior Software Engineer @ Arcesium",
+    "linkedinId":"Your LinkedIn Id",
+    "roleDescription": "",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/shubhendu28/",
+          "className":"fa fa-linkedin"
+        }
+      ],
+    "aboutme":"I have been working in software industry for past three years. Currently i am working in Fintech industry. Previously, i have worked in health industry",
+    "address":"Hyderabad, India",
+    "emailId":"shubhendushishir628@gmail.com",
+    "education":[
+      {
+        "UniversityName":"NIT Jamshedpur",
+        "specialization":"Computer Science & Engineering",
+        "MonthOfPassing":"May",
+        "YearOfPassing":"2017",
+        "Achievements":"Rank 7th - ACM ICPC 2016 Kolkata Regionals"
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"Arcesium",
+        "specialization":"Senior Software Engineer",
+        "MonthOfLeaving":"April 2020 - Current",
+        "Achievements":"Some Achievements"
+      },
+      {
+        "CompanyName":"Optum | UHG",
+        "specialization":"Software Engineer",
+        "MonthOfLeaving":"July 2017 - April 2020",
+        "Achievements":"Some Achievements"
+      }
+    ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"Java"
+      },
+      {
+        "skillname":"JavaScript"
+      },
+      {
+        "skillname":"Spring"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"project1",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      },
+      {
+        "name":"project2",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/project.jpg"
+      },
+      {
+        "name":"project3",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/project2.png"
+      },
+      {
+        "name":"project4",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      }
+    ]
+  }
+  
+  export default resumeData
